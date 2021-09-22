66e6c76667dfaa60378cccc13b22c5744b8e4f6d
Matthew Roberts <thew.roberts@gmail.com>
2021-09-22 11:07:23 +0000

    New style front page

---
+++ media/videos/index.php
@@ -0,0 +1,51 @@
+<html>
+	<head>
+		<title>DCS Defenders Robotics: Videos</title>
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700|Open+Sans:300,400,400i,600,700,800&display=swap" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Alfa+Slab+One&display=swap" rel="stylesheet">
+		<link rel="stylesheet" href="/styles.css"/>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+		<script src="https://kit.fontawesome.com/edb9398a3c.js" crossorigin="anonymous"></script>
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+		<style media="screen">
+			.card-deck > .card {
+				max-width:24rem;
+				min-width:18rem;
+				margin-bottom:2rem;
+			}
+		</style>
+	</head>
+	<body>
+		<nav class="navbar navbar-dark bg-dcs-blue navbar-expand-lg fixed-top">
+			<a class="navbar-brand" href="#"><i class="fad fa-robot fa-2x"></i></a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="navbar-nav">
+					<li class="nav-item active">
+						<a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+		<div class="container-fluid mt-5rem">
+			<div class="row pt-4">
+				<div class="col-sm text-center"><h1 class="display-3">DCS Defenders Robotics</h1></div>
+			</div>
+			<div class="row">
+				<div class="col-12 col-md-6 ">
+					<div class="card  ml-md-2 mt-4 shadow">
+						<h5 class="card-header">Videos</h5>
+						<p class="m-3"><a href="IMG_0031.MOV">Testing season 1 bot</a></p>
+						<p class="m-3"><a href="MVI_6953.MP4">Season 1 Gull Lake competition match</a></p>
+						<p class="m-3"><a href="MVI_6966.MP4">Season 1 Gull Lake competition match (fail)</a></p>
+				
+					</div>
+				</div>
+		<div id="background"/>
+	</body>
+</html>
