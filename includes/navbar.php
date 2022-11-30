15d17d52e08b1af05a1a98646b39ffc1b9480bd6
Matthew Roberts <thew.roberts@gmail.com>
2022-11-30 15:55:51 +0000

    Fixed error with hamburger menu not appearing (toggler attribute tags were not properly named for most recent Boostrap)

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,7 +1,7 @@
-<nav class="navbar navbar-dark navbar-expand-lg fixed-top">
+<nav class="navbar navbar-dark navbar-expand-md fixed-top">
 	<div class="container-fluid">
 		<a class="navbar-brand" href="/"><i class="fad fa-robot fa-2x"></i></a>		
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
 		<div class="collapse navbar-collapse" id="navbarNav">
